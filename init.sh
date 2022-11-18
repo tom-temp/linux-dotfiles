3d80e866ca3c3255772956b59fc149895fad295f
test-ning <aaa@aaa.com>
2022-11-18 14:46:21 +0800

    git init

---
+++ init.sh
@@ -0,0 +1,8 @@
+
+git config --global core.autocrlf true
+git config --global core.autocrlf input
+git config --global core.autocrlf false
+
+git config --global core.safecrlf true
+git config --global core.safecrlf false
+git config --global core.safecrlf warn
