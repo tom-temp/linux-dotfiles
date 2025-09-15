6e0c42e62bc47742c9c3e59355b941569b831c95
test-ning <aaa@aaa.com>
2025-09-14 20:32:44 -0400

    zsh_gnome版，笔记本电脑使用，使用ohmyposh

---
+++ zsh_gnome/.config/zsh/custom_shellapp.zsh
@@ -0,0 +1,137 @@
+
+# 环境变量
+export PATH=$PATH:$HOME/.local/bin
+
+export COLOR_H1_0='\e[1;35;42m'
+export COLOR_H1_1='\e[4;30;46m'
+export COLOR_H2_0='\e[1;35;40m'
+export COLOR_H2_1='\e[1;32;40m'
+export COLOR_END='\e[0m'
+
+export GITUL=http://github.opsp.eu.org/
+
+# default programe
+export EDITOR='/usr/bin/vim'
+
+# package
+## xbps
+if [ "$(command -v xbps-install)" ]; then
+  alias xbps-install='sudo xbps-install -S && sudo xbps-install'
+fi
+
+## brew
+if [ -e "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+# theme =======================================================================================================================
+# sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+# sudo chmod +x /usr/local/bin/oh-my-posh
+if [ "$(command -v oh-my-posh)" ]; then
+  eval "$(oh-my-posh init zsh --config ~/.config/zsh/theme/timwhite.omp.toml )"
+fi
+# starship
+# if [ "$(command -v starship)" ]; then
+#     eval "$(starship init zsh)"
+# fi
+# atuin
+if [ "$(command -v atuin)" ]; then
+    export ATUIN_NOBIND="true"
+    eval "$(atuin init zsh)"
+    # eval "$(atuin init zsh --disable-up-arrow)"
+    bindkey '^f' atuin-search
+    # bindkey '^f' _atuin_search_widget # if atuin <18.0
+fi
+if [ "$(command -v fzf)" ]; then
+    # 使用默认程序打开
+    # open $(fzf)
+    # 发送至剪贴板
+    # fzf | tr -d '\n' | tee > (pbcopy)
+    # 多个输出
+    # fzf --multi --bind='ctrl-a:select-all'
+    # fzf --multi --bind='ctrl-a:select-all' --layout=reverse
+    # $ 以什么结尾
+    # ! 取反
+    export FZF_COMPLETION_TRIGGER='@@'
+    # export FZF_DEFAULT_OPTS='--height 40% --tmux bottom,40% --layout reverse --border top'
+    export FZF_CTRL_T_OPTS='
+    --height 50% --tmux bottom,50% --layout reverse --border top
+    --walker-skip .git,node_modules,target
+    --preview "[[ $(file --mime {}) =~ binary ]] && echo $COLOR_H1_1{}$COLOR_END is a binary file || bat -n --color always -r :30 {}"'
+    source <(fzf --zsh)
+fi
+
+
+# 命令设置 =======================================================================================================================
+HISTTIMEFORMAT="[%m-%d %H:%M] "
+
+alias rmdel='rm'
+# alias rm='adfadfadsfasdf'
+
+# ls命令
+if [ "$(command -v exa)" ]; then
+    alias ll='exa -l --color always -aa -s type --icons'
+    alias ls='exa -s type --icons'
+    alias tree='exa -T --icons -L 2 -s type'
+    alias lst='exa -T --icons -L 2 -s type'
+    alias lln='exa -l --color always -aa -s type '
+    alias lsn='exa -s type'
+fi
+# if [ "$(command -v lsd)" ]; then
+#     alias lsd='lsd -Al --group-dirs first'
+#     alias lsa='lsd -AXl --group-dirs first'
+#     alias lst='lsd --tree --group-dirs first --depth 2'
+# fi
+if [ "$(command -v zoxide)" ]; then
+    unalias zi || echo "zinit 没有设置zi别名"
+    eval "$(zoxide init zsh)"
+fi
+if [ "$(command -v dysk)" ]; then
+    alias lfs='dysk'
+fi
+if [ "$(command -v rg)" ]; then
+    alias grep='rg'
+fi
+if [ "$(command -v procs)" ]; then
+    alias psui='procs'
+fi
+if [ "$(command -v dust)" ]; then
+    alias du='dust'
+fi
+# if [ "$(command -v trash)" ]; then
+#     alias rm='trash'
+#     alias rml='trash-list'
+# fi
+if [ "$(command -v gio)" ]; then
+    alias rm='gio trash'
+fi
+
+if [ "$(command -v macchina)" ]; then
+    alias a='macchina -t Lithium'
+fi
+if [ "$(command -v yazi)" ]; then
+    function ya() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+        yazi "$@" --cwd-file="$tmp"
+        if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+            cd -- "$cwd"
+        fi
+        rmdel -f -- "$tmp"
+    }
+fi
+
+
+# rust环境变量 ==================================================================================================================
+export CARGO_HTTP_MULTIPLEXING=false
+export CARGO_HOME=${SAPP_DIR}/_env_make/rust-cargo
+export RUSTUP_HOME=${SAPP_DIR}/_env_make/rust-main
+CARGO=${SAPP_DIR}/_env_make/rust-cargo/bin
+# . "${SAPP_DIR}/_env_make/rust-cargo/env"
+
+# completion
+# sudo ln -s ${SAPP_DIR}/zoxide/completions/_zoxide /usr/share/zsh/site-functions/
+# source ${SAPP_DIR}/zoxide/completions/zoxide.bash
+# sudo ln -s $SAPP_DIR/lsd/autocomplete/_lsd /usr/share/zsh/site-functions
+
+alias supervisorctl="$WAPP_DIR/supervisord/supervisord ctl -s http://127.0.0.1:18080 -u tom -P  tom-tom@"
+
---
+++ zsh_gnome/.zshrc
@@ -0,0 +1,104 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+# fi
+
+# The following lines were added by compinstall
+# for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}; done
+
+# 补全提示
+autoload -U compinit; compinit
+# autoload -U +X bashcompinit && bashcompinit
+
+##提示排序
+zstyle ':completion:*:*:cp:*' file-sort size
+zstyle ':completion:*' file-sort modification
+zstyle ':completion:*' file-list all
+##补全选择
+zstyle ':completion:*' completer _extensions _complete _approximate
+zstyle ':completion:*' menu select
+##分类
+zstyle ':completion:*:*:*:*:descriptions' format '%F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+
+# 信息
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{red}!- %d (errors: %e) -!%f'
+
+# 缓存
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "~/.config/zsh/.zcompcache"
+
+# End of lines added by compinstall
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.config/zsh/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+
+# man theme
+export LESS=-R
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;31m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;44;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;32m'
+
+# End of lines configured by zsh-newuser-install
+# mkdir -p ~/.config/zsh
+# git clone https://github.com/zdharma-continuum/zinit.git ~/.config/zsh/zinit
+source "$HOME/.config/zsh/zinit/zinit.zsh"
+autoload -Uz _zinit
+(( ${+_comps} )) && _comps[zinit]=_zinit
+zinit ice depth=1
+
+# custom app alias
+source ~/.config/zsh/custom_shellapp.zsh
+source ~/.config/zsh/zinit-mod.zsh
+source ~/.config/path_custom.zsh
+
+case "${TERM}" in
+  cons25*|linux) # plain BSD/Linux console
+    bindkey '\e[H'    beginning-of-line   # home
+    bindkey '\e[F'    end-of-line         # end
+    bindkey '\e[5~'   delete-char         # delete
+    bindkey '[D'      emacs-backward-word # esc left
+    bindkey '[C'      emacs-forward-word  # esc right
+    ;;
+
+  *rxvt*) # rxvt derivatives
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\eOc'    forward-word        # ctrl right
+    bindkey '\eOd'    backward-word       # ctrl left
+    # workaround for screen + urxvt
+    bindkey '\e[7~'   beginning-of-line   # home
+    bindkey '\e[8~'   end-of-line         # end
+    bindkey '^[[1~'   beginning-of-line   # home
+    bindkey '^[[4~'   end-of-line         # end
+    ;;
+
+  *xterm*) # xterm derivatives
+    bindkey '\e[H'    beginning-of-line   # home
+    bindkey '\e[F'    end-of-line         # end
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\e[1;5C' forward-word        # ctrl right
+    bindkey '\e[1;5D' backward-word       # ctrl left
+    # workaround for screen + xterm
+    bindkey '\e[1~'   beginning-of-line   # home
+    bindkey '\e[4~'   end-of-line         # end
+    ;;
+
+  screen)
+    bindkey '^[[1~'   beginning-of-line   # home
+    bindkey '^[[4~'   end-of-line         # end
+    bindkey '\e[3~'   delete-char         # delete
+    bindkey '\eOc'    forward-word        # ctrl right
+    bindkey '\eOd'    backward-word       # ctrl left
+    bindkey '^[[1;5C' forward-word        # ctrl right
+    bindkey '^[[1;5D' backward-word       # ctrl left
+    ;;
+esac
